346602644cad121f4431d8936349eae264c08d62
kajal <manekajal2014@gmail.com>
2021-10-05 15:29:27 +0530

    Tic Tac Toe Game

--- src/main/java/com/bl/TicTacToeGame.java
+++ src/main/java/com/bl/TicTacToeGame.java
@@ -1,8 +1,46 @@
 package com.bl;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+    static String user;
+    static String computer;
+    static char[] board = new char[10];
 
-    static char[] board= new char[10] ;
-    char arrayLength= (char) board.length;
+    static public char[] assign() {
 
+
+        for (int i = 0; i < board.length; i++) {
+            board[i] = ' ';
+
+        }
+        return board;
+
+    }
+    static public void createUser() {
+        Scanner scan =new Scanner(System.in);
+        System.out.println(" Please choose between X or O :");
+
+        System.out.println(" Enter X or O :");
+        String user= scan.nextLine();
+
+
+        if(user.equals("X")){
+
+            computer="O";
+            user="X";
+        }else{
+            user="X";
+            computer="O";
+
+        }
+    }
+
+    public static void main(String[] args){
+          assign();
+          createUser();
+
+        System.out.println(user);
+        System.out.println(computer);
+    }
 }
